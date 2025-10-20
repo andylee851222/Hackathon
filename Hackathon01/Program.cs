c91e160893a69213a815fd74e32a13c111683cb9
AndyLee <andy851222lee@gmail.com>
2025-10-20 10:10:34 +0800

    加入專案檔案。

---
+++ Hackathon01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Hackathon01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入一個大於 90 的整數：");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("Dann");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Build");
+                else if (i % 5 == 0)
+                    Console.WriteLine("School");
+                else
+                    Console.WriteLine(i);
+            }
+
+        }
+    }
+}
