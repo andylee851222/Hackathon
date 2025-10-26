7298af6668c33cf698d461be70dd705ea2181a42
Andy <Andy@DESKTOP-58MP4JI>
2025-10-26 16:28:03 +0800

    第三題

---
+++ Hackathon03/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Hackathon03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個英文敘述字串:");
+            Dictionary<string, int> wordCount  = WordCount.CountWords(Console.ReadLine());
+            foreach (var kvp in wordCount)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+        }
+    }
+}
---
+++ Hackathon03/WordCount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon03
+{
+    internal class WordCount
+    {
+        public static Dictionary<string, int> CountWords(string input)
+        {
+            // 轉換為小寫
+            string cleanedInput = input.ToLower();
+            string[] words = cleanedInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string,int> wordCount = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+            return wordCount;
+        }
+    }
+}
