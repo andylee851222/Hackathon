4426f770f8be54356cf6a95f89ac22dce2f61978
Andy <Andy@DESKTOP-58MP4JI>
2025-10-26 15:26:23 +0800

    第二題

---
+++ Hackathon02/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace homework02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.radioCtoF = new System.Windows.Forms.RadioButton();
+            this.radioFtoC = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConvert_Click = new System.Windows.Forms.Button();
+            this.btnClear_Click = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(443, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(321, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(381, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(44, 44);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(443, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(44, 44);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(321, 200);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(44, 44);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(381, 200);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(44, 44);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(443, 200);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(44, 44);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(321, 263);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(44, 44);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(381, 263);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(44, 44);
+            this.button11.TabIndex = 10;
+            this.button11.Text = ".";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // radioCtoF
+            // 
+            this.radioCtoF.AutoSize = true;
+            this.radioCtoF.Location = new System.Drawing.Point(23, 30);
+            this.radioCtoF.Name = "radioCtoF";
+            this.radioCtoF.Size = new System.Drawing.Size(83, 16);
+            this.radioCtoF.TabIndex = 11;
+            this.radioCtoF.TabStop = true;
+            this.radioCtoF.Text = "攝氏轉華氏";
+            this.radioCtoF.UseVisualStyleBackColor = true;
+            // 
+            // radioFtoC
+            // 
+            this.radioFtoC.AutoSize = true;
+            this.radioFtoC.Location = new System.Drawing.Point(23, 67);
+            this.radioFtoC.Name = "radioFtoC";
+            this.radioFtoC.Size = new System.Drawing.Size(83, 16);
+            this.radioFtoC.TabIndex = 12;
+            this.radioFtoC.TabStop = true;
+            this.radioFtoC.Text = "華氏轉攝氏";
+            this.radioFtoC.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(531, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 22);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnConvert_Click
+            // 
+            this.btnConvert_Click.Location = new System.Drawing.Point(531, 211);
+            this.btnConvert_Click.Name = "btnConvert_Click";
+            this.btnConvert_Click.Size = new System.Drawing.Size(91, 33);
+            this.btnConvert_Click.TabIndex = 14;
+            this.btnConvert_Click.Text = "轉換";
+            this.btnConvert_Click.UseVisualStyleBackColor = true;
+            this.btnConvert_Click.Click += new System.EventHandler(this.btnConvert_Click_Click);
+            // 
+            // btnClear_Click
+            // 
+            this.btnClear_Click.Location = new System.Drawing.Point(649, 211);
+            this.btnClear_Click.Name = "btnClear_Click";
+            this.btnClear_Click.Size = new System.Drawing.Size(91, 33);
+            this.btnClear_Click.TabIndex = 15;
+            this.btnClear_Click.Text = "清除";
+            this.btnClear_Click.UseVisualStyleBackColor = true;
+            this.btnClear_Click.Click += new System.EventHandler(this.btnClear_Click_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioFtoC);
+            this.groupBox1.Controls.Add(this.radioCtoF);
+            this.groupBox1.Location = new System.Drawing.Point(32, 114);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(538, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "結果:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnClear_Click);
+            this.Controls.Add(this.btnConvert_Click);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.RadioButton radioCtoF;
+        private System.Windows.Forms.RadioButton radioFtoC;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConvert_Click;
+        private System.Windows.Forms.Button btnClear_Click;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hackathon02/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace homework02
+{
+    public partial class Form1 : Form
+    {
+        double tempC = 0;
+        double tempF = 0;
+        string inputText = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnConvert_Click_Click(object sender, EventArgs e)
+        {
+            if (radioCtoF.Checked == true)
+            {
+                tempC = double.Parse(inputText);
+                tempF = (tempC * 9 / 5) + 32;
+                label1.Text = "結果: " + tempF.ToString();
+
+
+            }
+            else if (radioFtoC.Checked == true)
+            {
+                tempF = double.Parse(inputText);
+                tempC = (tempF - 32) * 5 / 9;
+                label1.Text = "結果: " + tempC.ToString();
+            }
+            else
+            {
+                MessageBox.Show("請選擇一個選項");
+                return;
+            }
+
+        }
+
+        private void btnClear_Click_Click(object sender, EventArgs e)
+        {
+            // clear
+            inputText = string.Empty;
+            textBox1.Text = string.Empty;
+            label1.Text = string.Empty;
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "1";
+            textBox1.Text = inputText;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "2";
+            textBox1.Text = inputText;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "3";
+            textBox1.Text = inputText;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "4";
+            textBox1.Text = inputText;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "5";
+            textBox1.Text = inputText;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "6";
+            textBox1.Text = inputText;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "7";
+            textBox1.Text = inputText;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "8";
+            textBox1.Text = inputText;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "9";
+            textBox1.Text = inputText;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + "0";
+            textBox1.Text = inputText;
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            inputText = inputText + ".";
+            textBox1.Text = inputText;
+        }
+    }
+}
